d29bf9e71662708e94619949d4639d0e504afdba
Mohd Arsla <mohdarslan626@gmail.com>
2023-12-18 19:21:27 +0530

    Added Exception handling,  debugging and connencting templates with views

--- myprblog/blog/views.py
+++ myprblog/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Post
 
 def home(request):
@@ -11,5 +12,17 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    try:
+        post = get_object_or_404(Post, pk=pk)
+        print(f"Post: {post}")
+        return render(request, 'blog/post_detail.html', {'post': post})
+    except Http404:
+        print("Post not found!")
+        return render(request, 'blog/404.html', status=404)
+    except Exception as e:
+        # Log the exception
+        print(f"Error in post_detail view: {e}")
+        logger.exception(f"Error in post_detail view: {e}")
+        # Render a generic error page
+        return render(request, 'blog/error.html', {'error_message': str(e)}, status=500)
+
